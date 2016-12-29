50d8fe49ae930bd1d59a3bc9a68a1e680bad4345
lsySummer <864986781@qq.com>
2016-12-29 09:51:34 +0800

    。

--- src/com/geariot/platform/yi/controller/WeChatController.java
+++ src/com/geariot/platform/yi/controller/WeChatController.java
@@ -7,7 +7,6 @@ import java.net.URLEncoder;
 import org.apache.log4j.Logger;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.jsoup.select.Evaluator.IsEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
