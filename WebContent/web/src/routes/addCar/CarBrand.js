13e11166ce6ee2f32ef3b5b9ff778b2cdb29b028
IDoRemember <1015885915@qq.com>
2017-08-14 17:53:31 +0800

    车辆品牌

--- WebContent/web/src/routes/addCar/CarBrand.js
+++ WebContent/web/src/routes/addCar/CarBrand.js
@@ -1,5 +1,5 @@
 import { province } from 'antd-mobile-demo-data';
-import { ListView, List, Flex } from 'antd-mobile';
+import { ListView, List, Flex,Drawer} from 'antd-mobile';
 import NavBar from '../../components/NavBar'
 import DasAuto from '../../img/das_auto.jpg'
 import fute from '../../img/fute.jpg'
@@ -33,6 +33,7 @@ class SelectCarBrand extends React.Component {
         this.state = {
             dataSource: dataSource.cloneWithRowsAndSections(dataBlob, sectionIDs, rowIDs),
             headerPressCount: 0,
+            open:true,
             hotlist: [{ img: DasAuto, name: '大众' }, { img:fute, name: '福特' }, { img: '', name: '本田' }, { img: '', name: '丰田' }, { img: '', name: '别克' }, { img: '', name: '奥迪' }]
         };
     }
@@ -42,13 +43,29 @@ class SelectCarBrand extends React.Component {
         console.log(this.state.dataSource)
     }
 
-    render() {
+    onOpenChange = (...args) => {
+        console.log(args);
+        this.setState({ open: !this.state.open });
+    }
+
+    render() { 
         let hotlist = this.state.hotlist.map((item, index) => {
         return <Flex key={index} className={index>4?'one-item border-no':'one-item'} direction="column">
                 <img src={item.img} alt="" />
                 <div>{item.name}</div>
             </Flex>
         })
+        const sidebar = (<List>
+            {[...Array(20).keys()].map((i, index) => {
+              if (index === 0) {
+                return (<List.Item key={index}
+                  multipleLine
+                >Category</List.Item>);
+              }
+              return (<List.Item key={index}
+              >Category{index}</List.Item>);
+            })}
+          </List>);
         return (
             <div className="body-bac">
                 <NavBar title="请选择品牌" />
@@ -64,8 +81,7 @@ class SelectCarBrand extends React.Component {
                     </div>}
                     renderFooter={() => <span>custom footer</span>}
                     renderSectionHeader={sectionData => (<div className="ih">{sectionData}</div>)}
-                    renderRow={rowData => (<Item>{rowData}</Item>)}
-                    
+                    renderRow={rowData => (<Item onClick={(e)=>{this.onOpenChange(e)}}>{rowData}</Item>)}
                     style={{
                         height: document.documentElement.clientHeight ,
                         overflow: 'auto',
@@ -77,6 +93,17 @@ class SelectCarBrand extends React.Component {
                     delayTime={10}
                     delayActivityIndicator={<div style={{ padding: 25, textAlign: 'center' }}>rendering...</div>}
                 />
+                <Drawer
+                    className="my-drawer"
+                    style={{ minHeight: document.documentElement.clientHeight - 200 }}
+                    enableDragHandle
+                    position={"right"}
+                    contentStyle={{ color: '#A6A6A6', textAlign: 'center', paddingTop: 42 }}
+                    sidebar={sidebar}
+                    open={this.state.open}
+                    onOpenChange={(e)=>{this.onOpenChange(e)}}
+                >
+                </Drawer>
             </div>
         );
     }
