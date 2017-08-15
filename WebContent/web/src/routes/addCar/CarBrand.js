82f9abdc168860a9a1d67c9514c08f5ecee16d3b
IDoRemember <1015885915@qq.com>
2017-08-15 13:39:18 +0800

    增加车辆

--- WebContent/web/src/routes/addCar/CarBrand.js
+++ WebContent/web/src/routes/addCar/CarBrand.js
@@ -1,5 +1,5 @@
 import { province } from 'antd-mobile-demo-data';
-import { ListView, List, Flex,Drawer} from 'antd-mobile';
+import { ListView, List, Flex, Drawer } from 'antd-mobile';
 import NavBar from '../../components/NavBar'
 import DasAuto from '../../img/das_auto.jpg'
 import fute from '../../img/fute.jpg'
@@ -33,8 +33,8 @@ class SelectCarBrand extends React.Component {
         this.state = {
             dataSource: dataSource.cloneWithRowsAndSections(dataBlob, sectionIDs, rowIDs),
             headerPressCount: 0,
-            open:true,
-            hotlist: [{ img: DasAuto, name: '大众' }, { img:fute, name: '福特' }, { img: '', name: '本田' }, { img: '', name: '丰田' }, { img: '', name: '别克' }, { img: '', name: '奥迪' }]
+            open: false,
+            hotlist: [{ img: DasAuto, name: '大众' }, { img: fute, name: '福特' }, { img: '', name: '本田' }, { img: '', name: '丰田' }, { img: '', name: '别克' }, { img: '', name: '奥迪' }]
         };
     }
 
@@ -48,61 +48,59 @@ class SelectCarBrand extends React.Component {
         this.setState({ open: !this.state.open });
     }
 
-    render() { 
+    render() {
         let hotlist = this.state.hotlist.map((item, index) => {
-        return <Flex key={index} className={index>4?'one-item border-no':'one-item'} direction="column">
+            return <Flex key={index} className={index > 4 ? 'one-item border-no' : 'one-item'} direction="column">
                 <img src={item.img} alt="" />
                 <div>{item.name}</div>
             </Flex>
         })
         const sidebar = (<List>
             {[...Array(20).keys()].map((i, index) => {
-              if (index === 0) {
-                return (<List.Item key={index}
-                  multipleLine
-                >Category</List.Item>);
-              }
-              return (<List.Item key={index}
-              >Category{index}</List.Item>);
+                if (index === 0) {
+                    return (<List.Item key={index}
+                        multipleLine
+                    ><img src={DasAuto} />大众</List.Item>);
+                }
+                return (<List.Item key={index} onClick={(e)=>{this.onOpenChange(e)}}
+                >Category{index}</List.Item>);
             })}
-          </List>);
+        </List>);
         return (
             <div className="body-bac">
                 <NavBar title="请选择品牌" />
-
-                <ListView.IndexedList
-                    className="car-brand-header-box "
-                    dataSource={this.state.dataSource}
-                    renderHeader={() => <div>
-                        <div style={{ width: '100%',  color: '#808080', background: '#fff', height: '.6rem', lineHeight: '.6rem',marginBottom:'2px' }}><span style={{marginLeft:'12px'}}>热门品牌</span></div>
-                        <Flex className="car-brand-head" wrap="wrap">
-                            {hotlist}
-                        </Flex>
-                    </div>}
-                    renderFooter={() => <span>custom footer</span>}
-                    renderSectionHeader={sectionData => (<div className="ih">{sectionData}</div>)}
-                    renderRow={rowData => (<Item onClick={(e)=>{this.onOpenChange(e)}}>{rowData}</Item>)}
-                    style={{
-                        height: document.documentElement.clientHeight ,
-                        overflow: 'auto',
-                    }}
-                    quickSearchBarStyle={{
-                        position: 'absolute',
-                        top: 150,
-                    }}
-                    delayTime={10}
-                    delayActivityIndicator={<div style={{ padding: 25, textAlign: 'center' }}>rendering...</div>}
-                />
                 <Drawer
                     className="my-drawer"
                     style={{ minHeight: document.documentElement.clientHeight - 200 }}
-                    enableDragHandle
                     position={"right"}
-                    contentStyle={{ color: '#A6A6A6', textAlign: 'center', paddingTop: 42 }}
+                    contentStyle={{ color: '#1c1c1c', textAlign: 'center', paddingTop: 42 }}
                     sidebar={sidebar}
                     open={this.state.open}
-                    onOpenChange={(e)=>{this.onOpenChange(e)}}
+                    onOpenChange={(e) => { this.onOpenChange(e) }}
                 >
+                    <ListView.IndexedList
+                        className="car-brand-header-box "
+                        dataSource={this.state.dataSource}
+                        renderHeader={() => <div>
+                            <div style={{ width: '100%', color: '#808080', background: '#fff', height: '.6rem', lineHeight: '.6rem', marginBottom: '2px' }}><span style={{ marginLeft: '12px' }}>热门品牌</span></div>
+                            <Flex className="car-brand-head" wrap="wrap">
+                                {hotlist}
+                            </Flex>
+                        </div>}
+                        renderFooter={() => <span >custom footer</span>}
+                        renderSectionHeader={sectionData => (<div>{sectionData}</div>)}
+                        renderRow={rowData => (<Item onClick={(e) => { this.onOpenChange(e) }}>{rowData}</Item>)}
+                        style={{
+                            height: document.documentElement.clientHeight,
+                            overflow: 'auto',
+                        }}
+                        quickSearchBarStyle={{
+                            position: 'absolute',
+                            top: 150,
+                        }}
+                        delayTime={10}
+                        delayActivityIndicator={<div style={{ padding: 25, textAlign: 'center' }}>rendering...</div>}
+                    />
                 </Drawer>
             </div>
         );
---
+++ WebContent/web/src/routes/membership/MyCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../../components/NavBar'
+class MyCard extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            paystate: 0,
+            carState: 0,
+        }
+    }
+    render() {
+    }
+}
