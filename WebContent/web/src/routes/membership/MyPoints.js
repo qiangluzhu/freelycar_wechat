41a98a1760443485d385d3d06bb8188c05c21627
IDoRemember <1015885915@qq.com>
2017-08-16 10:14:08 +0800

    swiper

--- WebContent/web/src/routes/membership/MyPoints.js
+++ WebContent/web/src/routes/membership/MyPoints.js
@@ -10,13 +10,34 @@ class MyPoints extends React.Component {
     }
 
     render() {
-   
+
         return <div>
-              <NavBar title="我的积分" />
+            <NavBar title="我的积分" />
             <div className="my-total-point">
-                <Flex className="circle" align="center" direction="column" >
+                <Flex className="circle" justify="center" direction="column" >
                     <div>积分</div>
                     <div>2000</div>
+                </Flex>
+            </div>
+            <div className="my-total-point-content">
+                <div className="wait">
+                    敬请期待...
+                </div>
+                <div className="title">
+                    积分明细
+                </div>
+                <Flex className="detail">
+                    <Flex direction="column" align="start">
+                        <div className="comment">
+                            订单评价积分
+                        </div>
+                        <div className="time">
+                            2017.05.23
+                        </div>
+                    </Flex>
+                    <Flex.Item className="money">
+                        +25
+                    </Flex.Item>
                 </Flex>
             </div>
         </div>
