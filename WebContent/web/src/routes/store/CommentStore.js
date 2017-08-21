92849dc3e87624ebf5c3a768b32711b6efbc2262
IDoRemember <1015885915@qq.com>
2017-08-21 10:13:37 +0800

    合作门店的评价

--- WebContent/web/src/routes/store/CommentStore.js
+++ WebContent/web/src/routes/store/CommentStore.js
@@ -8,13 +8,13 @@ class CommentStore extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            nowStar:0
+            nowStar: 0
         }
     }
 
-    setScore=(number)=>{
+    setScore = (number) => {
         this.setState({
-            nowStar:number
+            nowStar: number
         })
     }
     componentDidMount() {
@@ -24,9 +24,18 @@ class CommentStore extends React.Component {
         return <div>
             <NavBar title="合作门店" />
             <div className="comment-store">
-
+                <Flex className="comment-store-title">
+                    <div className="comment-store-avatar"><img src="" alt="" /></div>
+                    <span className="title">小易爱车</span>
+                </Flex>
+                <Flex className="comment" justify="center">
+                    <CommentStar setScore={this.setScore} nowStar={this.state.nowStar} />
+                </Flex>
+                <textarea placeholder="亲，说出你的心里话，分享给大家吧(至少输入8个字)" className="comment-text">
+                </textarea>
             </div>
-            <CommentStar setScore={this.setScore} nowStar={this.state.nowStar}/>
+            <div className="comment-commit-btn">提交评价</div>
+
         </div>
     }
 
