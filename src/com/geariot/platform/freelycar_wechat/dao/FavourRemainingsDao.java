7da965ed81c32b1b15f1680c9e88c5707869c6e5
Tylerzhr <401469524@qq.com>
2017-09-01 15:54:27 +0800

    commit

--- src/com/geariot/platform/freelycar_wechat/dao/FavourRemainingsDao.java
+++
@@ -1,9 +0,0 @@
-package com.geariot.platform.freelycar_wechat.dao;
-
-import java.util.List;
-
-import com.geariot.platform.freelycar_wechat.entities.FavourRemainings;
-public interface FavourRemainingsDao {
-	public List<FavourRemainings> favourtByClientId(int clientId);
-	public Object getCountByClientId(int clientId);
-}
--- src/com/geariot/platform/freelycar_wechat/daoimpl/TicketDaoImpl.java
+++ src/com/geariot/platform/freelycar_wechat/daoimpl/TicketDaoImpl.java
@@ -5,14 +5,14 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.geariot.platform.freelycar_wechat.dao.FavourRemainingsDao;
-import com.geariot.platform.freelycar_wechat.entities.FavourRemainings;
+import com.geariot.platform.freelycar_wechat.dao.TicketDao;
+import com.geariot.platform.freelycar_wechat.entities.Ticket;
 import com.geariot.platform.freelycar_wechat.utils.Constants;
 
 import java.math.BigDecimal;
 import java.util.List;
 @Repository
-public class FavourRemainingsDaoImpl implements FavourRemainingsDao{
+public class TicketDaoImpl implements TicketDao{
 	@Autowired
 	private SessionFactory sessionFactory;
 	
@@ -22,14 +22,14 @@ public class FavourRemainingsDaoImpl implements FavourRemainingsDao{
 	
 	@SuppressWarnings("unchecked")
 	@Override
-	public List<FavourRemainings> favourtByClientId(int clientId) {
-		String hql = "from FavourRemainings where clientId = :clientId";
+	public List<Ticket> favourtByClientId(int clientId) {
+		String hql = "from Ticket where clientId = :clientId";
 		return this.getSession().createQuery(hql).setCacheable(Constants.SELECT_CACHE).setInteger("clientId",clientId).list();
 	}
 
 	@Override
 	public Object getCountByClientId(int clientId) {
-		String sql = "SELECT SUM(remaining) FROM `favourremainings` where clientId= :clientId";
+		String sql = "SELECT SUM(remaining) FROM `Ticket` where clientId= :clientId";
 		return  this.getSession().createSQLQuery(sql).setInteger("clientId", clientId).uniqueResult();
 	}
 
