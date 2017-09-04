956e3379daba307b08e245d1487b23f2b8eb9d6c
IDoRemember <1015885915@qq.com>
2017-09-04 11:52:08 +0800

    个人中心掉接口

--- WebContent/web/src/routes/personalCenter/Personalcenter.js
+++ WebContent/web/src/routes/personalCenter/Personalcenter.js
@@ -8,17 +8,47 @@ import order_icon from '../../img/order_icon.png'
 import Contactxiaoyi_icon from '../../img/Contactxiaoyi_icon.png'
 import avatar from '../../assets/yay.jpg'
 import banner from '../../img/member_banner.png'
+import { wxInfo,userDetail } from '../../services/user.js'
+import {browserHistory} from 'dva/router'
 class Personalcenter extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
-
+            point:0,
+            name:'',
+            headimgurl:'',
         }
     }
 
-    componentDidMount(){
-        
+    componentDidMount() {
+        wxInfo({
+            openId: '11'
+        }).then((res) => {
+            console.log(res)
+            if (res.data.code == '0') {
+                let data = res.data.data
+                this.setState({
+                    point: data.point,
+                    name: data.name ? data.name : data.nickName,
+                    headimgurl:data.headimgurl
+                })
+            }
+        }).catch((error)=>{console.log(error)})
+
+        userDetail({
+            clientId:'10'
+        }).then((res)=>{
+            console.log(res)
+            if(res.data.code == '0') {
+                let data = res.data.data
+                this.setState({
+                    card:data.cards
+                })
+            }
+        }).catch((error)=>{
+            console.log(error)
+        })
     }
     render() {
         return <div className="body-bac">
@@ -26,7 +56,7 @@ class Personalcenter extends React.Component {
             </div>
             <div className="clear"><div className="center-login-out"></div><a href="tel:18512391863" className="center-line-phone"></a></div>
             <Flex justify="between" align='start' direction="column" className="person-info">
-                <Flex justify="between" align='start' style={{height:'1.2rem'}} >
+                <Flex justify="between" align='start' style={{ height: '1.2rem' }} >
                     <div className="avatar"><img src={avatar} alt="" /></div>
                     <Flex.Item style={{ marginLeft: '.3rem' }} direction="column">
                         <div className="info-name">Anan托马斯</div>
@@ -35,20 +65,20 @@ class Personalcenter extends React.Component {
                         </Flex>
                     </Flex.Item>
                 </Flex>
-                <Flex justify="between" style={{width:'100%'}}>
-                    <Flex direction="column" justify="center" align="center" style={{width:'50%'}}>
-                        <div style={{fontSize:'.36rem',color:'#37cedc'}}>3<span style={{fontSize:'.16rem'}}>个</span></div>
-                        <div style={{fontSize:'.22rem',lineHeight:'.35rem',color:'#8e8e8e'}}>优惠</div>
+                <Flex justify="between" style={{ width: '100%' }}>
+                    <Flex direction="column" justify="center" align="center" style={{ width: '50%' }}>
+                        <div style={{ fontSize: '.36rem', color: '#37cedc' }}>3<span style={{ fontSize: '.16rem' }}>个</span></div>
+                        <div style={{ fontSize: '.22rem', lineHeight: '.35rem', color: '#8e8e8e' }}>优惠</div>
                     </Flex>
-                    <Flex direction="column" justify="center" align="center"  style={{width:'50%'}}>
-                        <div style={{fontSize:'.36rem',color:'#37cedc'}}>2000<span style={{fontSize:'.16rem'}}>分</span></div>
-                        <div style={{fontSize:'.22rem',lineHeight:'.35rem',color:'#8e8e8e'}}>积分</div>
+                    <Flex direction="column" justify="center" align="center" style={{ width: '50%' }}>
+                        <div style={{ fontSize: '.36rem', color: '#37cedc' }}>2000<span style={{ fontSize: '.16rem' }}>分</span></div>
+                        <div style={{ fontSize: '.22rem', lineHeight: '.35rem', color: '#8e8e8e' }}>积分</div>
                     </Flex>
                 </Flex>
             </Flex>
 
             <div className="center-banner"><img src={banner} alt="" /></div>
-            <Flex className="center-line-box">
+            <Flex className="center-line-box" onClick={()=>{browserHistory.push('/addcar')}}>
                 <div className="center-icon1"><img src={Vehiclemanagement_icon} alt="" /></div>
                 <p>爱车管理</p>
                 <Flex.Item className="vip-card-more"><img src={more_arrow} alt="" /></Flex.Item>
