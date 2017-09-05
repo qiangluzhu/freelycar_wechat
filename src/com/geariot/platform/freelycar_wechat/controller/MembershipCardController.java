548cbe7cbff6b2d34a0071e989aae681d30eb07e
Tylerzhr <401469524@qq.com>
2017-09-05 14:00:02 +0800

    commit

--- src/com/geariot/platform/freelycar_wechat/controller/MembershipCardController.java
+++ src/com/geariot/platform/freelycar_wechat/controller/MembershipCardController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.geariot.platform.freelycar_wechat.service.CardService;
 import com.geariot.platform.freelycar_wechat.service.MembershipCardService;
 
 @RestController
@@ -13,8 +12,6 @@ import com.geariot.platform.freelycar_wechat.service.MembershipCardService;
 public class MembershipCardController {
 	@Autowired
 	MembershipCardService menmbershipCardService;
-	@Autowired
-	CardService cardService;
 	
 	@RequestMapping(value = "/list" , method = RequestMethod.GET)
 	public String getMembershipCardList(int page , int number){
