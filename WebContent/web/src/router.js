057d5549c7578341de9ecc94e022672366029b32
IDoRemember <1015885915@qq.com>
2017-09-11 11:37:23 +0800

    合作门店列表页

--- WebContent/web/src/router.js
+++ WebContent/web/src/router.js
@@ -63,7 +63,7 @@ function RouterConfig({ history }) {
 
       <Route path="/carInfo" component={CarInfo}/>
       <Route path="/store/cooperative-store" component={CooperativeStore}/>
-      <Route path="/store/comment" component={CommentStore}/>
+      <Route path="/store/comment/:consumerOrderId" component={CommentStore}/>
       <Route path="/store/detail" component={StoreDetail}/>
       <Route path="/insurance/:clientId/:carId" component={Insurance}/>
       <Route path="/insurance/inquiry" component={Inquiry}/>
