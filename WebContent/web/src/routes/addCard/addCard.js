02b59059e78ef7fd0f379c6d61a527971cba0dab
Javebean <448267696@qq.com>
2017-09-15 17:41:39 +0800

    commit

--- WebContent/web/src/routes/addCard/addCard.js
+++ WebContent/web/src/routes/addCard/addCard.js
@@ -35,6 +35,7 @@ class OrderDetail extends React.Component {
             page: window.location.href,
             number: 22
         }).then((res) => {
+            console.log(res);
             //先注入配置JSSDK信息
             wx.config({
                 debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
--- WebContent/web/src/services/pay.js
+++ WebContent/web/src/services/pay.js
@@ -11,12 +11,10 @@ export default {
         return get('api/pay/payment',option)
     },
 
-<<<<<<< HEAD
     activity:(option)=>{
         return get('api/pay/activity',option)
     },
-=======
->>>>>>> fe5926f33a558b73a88ab2c1a9a380462a8c13f8
+    
     getWXConfig:(option) =>{
         return get('api/pay/wx/getJSSDKConfig',option)
     }
