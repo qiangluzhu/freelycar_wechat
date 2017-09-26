ed148b3d337f73a905e032a040ede59f4937be16
IDoRemember <1015885915@qq.com>
2017-09-26 23:13:08 +0800

    commit

--- WebContent/web/src/routes/order/OrderTrack.js
+++ WebContent/web/src/routes/order/OrderTrack.js
@@ -19,7 +19,8 @@ class OrderTrack extends React.Component {
             clientName: '',
             deliverTime: '',
             finishTime: '',
-            createDate: ''
+            createDate: '',
+            empty: false
         }
     }
 
@@ -48,7 +49,7 @@ class OrderTrack extends React.Component {
         }).catch((error) => { console.log(error) });
 
         if (this.props.match.params.id == '$') {
-            if(window.localStorage.getItem('openid')) {
+            if (window.localStorage.getItem('openid')) {
                 quickOrder({
                     clientId: window.localStorage.getItem('clientId')
                 }).then((res) => {
@@ -68,14 +69,18 @@ class OrderTrack extends React.Component {
                             payState: data.payState,
                             stars: data.stars,
                             totalPrice: data.totalPrice,
-                            id:data.id
+                            id: data.id
                         })
                         window.localStorage.setItem('storeName', data.store.name)
                         window.localStorage.setItem('imgUrl', data.store.imgUrls[0])
+                    } else if (res.data.code == '2') {
+                        this.setState({
+                            empty: true
+                        })
                     }
                 }).catch((error) => { console.log(error) })
-            } 
-           
+            }
+
         } else {
             orderDetail({
                 consumOrderId: this.props.match.params.id
@@ -96,7 +101,7 @@ class OrderTrack extends React.Component {
                         projects: data.projects,
                         payState: data.payState,
                         stars: data.stars,
-                        id:data.id,
+                        id: data.id,
                         totalPrice: data.totalPrice
                     })
                     window.localStorage.setItem('storeName', data.store.name)
@@ -214,100 +219,103 @@ class OrderTrack extends React.Component {
                 订单跟踪
             <i className="scan"></i>
             </div>
-            <Flex className="order-track-baseinfo">
-                <Flex.Item className="Info">
-                    <p>{this.state.clientName}{window.localStorage.getItem('isMember') ? '(会员)' : ''}</p>
-                    <div>{this.state.licensePlate}</div>
-                </Flex.Item>
-                <Flex.Item className="state">{this.state.state == 1 ? '已接车' : (this.state.state == 2 ? '已完工' : '已交车')}</Flex.Item>
-            </Flex>
-            <div className="order-track-line"></div>
-            <Flex className="order-track-program" direction="column">
-                <div className="beauty special-font-size">
-                    <p>{this.state.programName}</p>
-                </div>
-                <div className="ordertrack-project">{projectItems}</div>
-
-                <div style={{ textAlign: 'right', width: '100%', marginBottom: '.1rem' }}><div style={{ marginRight: '.42rem', display: 'inline-block' }}>合计：<span style={{ fontSize: '.24rem', color: '#e42f2f' }}>￥</span><span style={{ color: '#e42f2f' }}>{totalPrice}</span></div>
-                    {this.state.payState == 0 && <div className="pay-btn" onClick={() => { this.handlePay(totalPrice) }}>
-                        <p>去付款</p>
-                    </div>}
-                </div>
-            </Flex>
-
-            <div className='order-list order-tarck-info'>
-                <Flex style={{ height: '100%' }}>
-                    <Flex.Item className='leftLable'>订单编号</Flex.Item>
-                    <Flex.Item className='rightText'>{this.state.id}</Flex.Item>
+            {!this.state.empty && <div>
+                <Flex className="order-track-baseinfo">
+                    <Flex.Item className="Info">
+                        <p>{this.state.clientName}{window.localStorage.getItem('isMember') ? '(会员)' : ''}</p>
+                        <div>{this.state.licensePlate}</div>
+                    </Flex.Item>
+                    <Flex.Item className="state">{this.state.state == 1 ? '已接车' : (this.state.state == 2 ? '已完工' : '已交车')}</Flex.Item>
                 </Flex>
-            </div>
-            <div className='order-list order-tarck-info' style={{ marginTop: '0', borderTop: '1px dashed #f0f0f0' }}>
-                <Flex style={{ height: '100%' }}>
-                    <Flex.Item className='leftLable'>订单时间</Flex.Item>
-                    <Flex.Item className='rightText'>{this.state.createDate}</Flex.Item>
-                </Flex>
-            </div>
-            <div className="order-track-state">
-                <Flex className={this.state.state == 3 ? "order-track-state-box active" : "order-track-state-box"} align="start">
-                    <div className="time">
-                        <p>{this.state.deliverTime ? this.state.deliverTime.slice(5, 10) : ''}</p>
-                        <p style={{ marginLeft: '.22rem' }}>{this.state.deliverTime ? this.state.deliverTime.slice(11, 16) : ''}</p>
+                <div className="order-track-line"></div>
+                <Flex className="order-track-program" direction="column">
+                    <div className="beauty special-font-size">
+                        <p>{this.state.programName}</p>
                     </div>
-                    <div className="right-box">
-                        <div className="circle">
-                        </div>
-                        <div className="car-state">已交车
-                        </div>
-                        <div>爱车已交回你的手中 快来评价获积分吧
-                        </div>
-                        {this.state.state == 3 && this.state.payState > 0 && this.state.stars == 0 && <div className="evaluate" onClick={() => { this.context.router.history.push(`/store/comment/${this.props.match.params.id}`) }}>
-                            评价得{commentPrice}积分
+                    <div className="ordertrack-project">{projectItems}</div>
+
+                    <div style={{ textAlign: 'right', width: '100%', marginBottom: '.1rem' }}><div style={{ marginRight: '.42rem', display: 'inline-block' }}>合计：<span style={{ fontSize: '.24rem', color: '#e42f2f' }}>￥</span><span style={{ color: '#e42f2f' }}>{totalPrice}</span></div>
+                        {this.state.payState == 0 && <div className="pay-btn" onClick={() => { this.handlePay(totalPrice) }}>
+                            <p>去付款</p>
                         </div>}
                     </div>
                 </Flex>
-                <Flex className={this.state.state == 2 ? "order-track-state-box active" : "order-track-state-box"} align="start">
-                    <div className="time">
-                        <p>{this.state.finishTime ? this.state.finishTime.slice(5, 10) : ''}</p>
-                        <p style={{ marginLeft: '.22rem' }}>{this.state.finishTime ? this.state.finishTime.slice(11, 16) : ''}</p>
-                    </div>
-                    <div className="right-box">
-                        <div className="circle">
+
+                <div className='order-list order-tarck-info'>
+                    <Flex style={{ height: '100%' }}>
+                        <Flex.Item className='leftLable'>订单编号</Flex.Item>
+                        <Flex.Item className='rightText'>{this.state.id}</Flex.Item>
+                    </Flex>
+                </div>
+                <div className='order-list order-tarck-info' style={{ marginTop: '0', borderTop: '1px dashed #f0f0f0' }}>
+                    <Flex style={{ height: '100%' }}>
+                        <Flex.Item className='leftLable'>订单时间</Flex.Item>
+                        <Flex.Item className='rightText'>{this.state.createDate}</Flex.Item>
+                    </Flex>
+                </div>
+                <div className="order-track-state">
+                    <Flex className={this.state.state == 3 ? "order-track-state-box active" : "order-track-state-box"} align="start">
+                        <div className="time">
+                            <p>{this.state.deliverTime ? this.state.deliverTime.slice(5, 10) : ''}</p>
+                            <p style={{ marginLeft: '.22rem' }}>{this.state.deliverTime ? this.state.deliverTime.slice(11, 16) : ''}</p>
                         </div>
-                        <div className="car-state">已完工
+                        <div className="right-box">
+                            <div className="circle">
+                            </div>
+                            <div className="car-state">已交车
                         </div>
-                        <div>您的汽车已爱护完毕 期待您的带回
+                            <div>爱车已交回你的手中 快来评价获积分吧
                         </div>
-                    </div>
-                </Flex>
-                <Flex className={this.state.state == 1 ? "order-track-state-box active" : "order-track-state-box"} align="start">
-                    <div className="time">
-                        <p>{this.state.createDate ? this.state.createDate.slice(5, 10) : ''}</p>
-                        <p style={{ marginLeft: '.22rem' }}>{this.state.createDate ? this.state.createDate.slice(11, 16) : ''}</p>
-                    </div>
-                    <div className="right-box">
-                        <div className="circle">
+                            {this.state.state == 3 && this.state.payState > 0 && this.state.stars == 0 && <div className="evaluate" onClick={() => { this.context.router.history.push(`/store/comment/${this.props.match.params.id}`) }}>
+                                评价得{commentPrice}积分
+                        </div>}
                         </div>
-                        <div className="car-state">商家已接车
+                    </Flex>
+                    <Flex className={this.state.state == 2 ? "order-track-state-box active" : "order-track-state-box"} align="start">
+                        <div className="time">
+                            <p>{this.state.finishTime ? this.state.finishTime.slice(5, 10) : ''}</p>
+                            <p style={{ marginLeft: '.22rem' }}>{this.state.finishTime ? this.state.finishTime.slice(11, 16) : ''}</p>
                         </div>
-                        <div>汽车已入库
+                        <div className="right-box">
+                            <div className="circle">
+                            </div>
+                            <div className="car-state">已完工
                         </div>
-                    </div>
-                </Flex>
-                <Flex className={this.state.state == 1 ? "order-track-state-box active" : "order-track-state-box"} align="start">
-                    <div className="time">
-                        <p>{this.state.createDate ? this.state.createDate.slice(5, 10) : ''}</p>
-                        <p style={{ marginLeft: '.22rem' }}>{this.state.createDate ? this.state.createDate.slice(11, 16) : ''}</p>
-                    </div>
-                    <div className="right-box last">
-                        <div className="circle">
+                            <div>您的汽车已爱护完毕 期待您的带回
                         </div>
-                        <div className="car-state">已下单
                         </div>
-                        <div>
+                    </Flex>
+                    <Flex className={this.state.state == 1 ? "order-track-state-box active" : "order-track-state-box"} align="start">
+                        <div className="time">
+                            <p>{this.state.createDate ? this.state.createDate.slice(5, 10) : ''}</p>
+                            <p style={{ marginLeft: '.22rem' }}>{this.state.createDate ? this.state.createDate.slice(11, 16) : ''}</p>
                         </div>
-                    </div>
-                </Flex>
-            </div>
+                        <div className="right-box">
+                            <div className="circle">
+                            </div>
+                            <div className="car-state">商家已接车
+                        </div>
+                            <div>汽车已入库
+                        </div>
+                        </div>
+                    </Flex>
+                    <Flex className={this.state.state == 1 ? "order-track-state-box active" : "order-track-state-box"} align="start">
+                        <div className="time">
+                            <p>{this.state.createDate ? this.state.createDate.slice(5, 10) : ''}</p>
+                            <p style={{ marginLeft: '.22rem' }}>{this.state.createDate ? this.state.createDate.slice(11, 16) : ''}</p>
+                        </div>
+                        <div className="right-box last">
+                            <div className="circle">
+                            </div>
+                            <div className="car-state">已下单
+                        </div>
+                            <div>
+                            </div>
+                        </div>
+                    </Flex>
+                </div>
+            </div>}
+            {this.state.empty&&<div className="empty-bac" ></div>}
         </div>
     }
 }
